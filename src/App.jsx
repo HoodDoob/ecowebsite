280520cea79570a5c9b35b93b7ad0d5e679e7388
Panni Belevski <pannibel@gmail.com>
2022-11-21 11:32:21 +0100

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { useState } from "react";
-<<<<<<< HEAD
-import "./App.css";
-=======
 import "./App.scss";
->>>>>>> c9598047fc5c08888d2750419f8ec29544502d53
 
 function App() {
   const [count, setCount] = useState(0);
