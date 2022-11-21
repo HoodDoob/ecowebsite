3f961a93e0f4c6fe3543a6f0b3d207acdf869e6e
HoodDoob <hubert.kurkiewicz@gmail.com>
2022-11-21 11:20:17 +0100

    some changes let's see

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import "./App.scss";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return;
+  <div className="App"></div>;
 }
 
 export default App;
