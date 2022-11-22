3a5ba522b2e934997bfbaedda0d1e45dc358b228
HoodDoob <hubert.kurkiewicz@gmail.com>
2022-11-22 18:21:07 +0100

    panni i s bak again

--- src/components/FormLayout.jsx
+++ src/components/FormLayout.jsx
@@ -6,26 +6,8 @@ import Results from "./Results";
 import { motion, AnimatePresence } from "framer-motion";
 
 function FormLayout() {
-  let pageCount = 0;
-  const [thisPage, setThisPage] = useState(pageCount);
   const [formFirstState, setFormFirstState] = useState(true);
 
-  // const transition = useTransition(thisPage, {
-  //   from: {},
-  //   enter: {},
-  //   leave: {},
-  // });
-  console.log(thisPage);
-  // function formCalculate() {
-  //   pageCount++;
-  //   setThisPage(pageCount);
-  //   console.log(thisPage);
-  // }
-  // function formGoBack() {
-  //   pageCount--;
-  //   setThisPage(pageCount);
-  //   console.log(thisPage);
-  // }
   function formCalculate() {
     setFormFirstState(!formFirstState);
     console.log("change the state to " + formFirstState);
@@ -34,10 +16,6 @@ function FormLayout() {
 
   return (
     <>
-      {/* {thisPage <= 0 && (
-        <Form setData={setData} formCalculate={formCalculate} />
-      )} */}
-
       {formFirstState ? (
         <AnimatePresence>
           <motion.div
@@ -52,7 +30,6 @@ function FormLayout() {
             transition={{ duration: 0.5 }}
             exit={{ x: "-150%", opacity: 1, scale: 1 }}
             initial={{ x: "-150%", opacity: 1, scale: 1 }}
-            // variants={variants}
           >
             <Form setData={setData} formCalculate={formCalculate} />
           </motion.div>{" "}
@@ -71,14 +48,11 @@ function FormLayout() {
               scale: 1,
               transition: { duration: 0.5 },
             }}
-            // variants={variants}
           >
             <Results data={data} formCalculate={formCalculate} />
           </motion.div>
         </AnimatePresence>
       )}
-
-      {/* {thisPage === 1 && <Results data={data} formGoBack={formGoBack} />} */}
     </>
   );
 }
