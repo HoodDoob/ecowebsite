ce82cc1fd1bc37c478681c95c01e2a1d6a91c630
HoodDoob <hubert.kurkiewicz@gmail.com>
2022-11-24 16:58:54 +0100

    the guy changes its size

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import {useState} from 'react';
+import { useState } from "react";
 
 function Results(props) {
   console.log(props.data);
+  let someValue = 65 * (8.2 / Math.cbrt(props.output * 0.552));
+  console.log(someValue);
+  const myStyle = { height: someValue };
 
-/*   function calculating() {  
+  /*   function calculating() {  
         data.filter((object) => {
         const amountTotal = object.amount;
         const timeTotal = object.time;
@@ -18,37 +21,38 @@ function Results(props) {
       });
     } */
 
-  function testCube1() {
-    const cube = document.querySelector(".cube");
-    const face = document.querySelectorAll(".cube__face");
-    // face.forEach((face) => {
-    //   face.style.height = "50px";
-    //   face.style.width = "100px";
-    // });
-
-    cube.classList.add("show-top");
-  }
+  // function testCube1() {
+  //   document.getElementById("horseman").height = "50px";
+  // }
+  // testCube1();
   return (
     <div>
       <h2 id="resultHeading">Your total CO2 footprint</h2>
       <div className="cubeGrid">
-        <img src="../src/assets/manOnAHorseWhite.svg" alt="horseman"></img>
+        <img
+          style={myStyle}
+          id="horseman"
+          src="../src/assets/manOnAHorseWhite.svg"
+          alt="horseman"
+        ></img>
         <div className="scene">
           <div className="cube">
             <div className="cube__face cube__face--front">
-            {props.output}kg<p>Cube of Doom®</p>
+              {props.output}kg<p>Cube of Doom®</p>
             </div>
             <div className="cube__face cube__face--back">
-            {props.output}kg <p>Cube of Doom®</p>
+              {props.output}kg <p>Cube of Doom®</p>
             </div>
             <div className="cube__face cube__face--right">
-            {props.output}kg<p>Cube of Doom®</p>
+              {props.output}kg<p>Cube of Doom®</p>
             </div>
             <div className="cube__face cube__face--left">
-            {props.output}kg<p>Cube of Doom®</p>
+              {props.output}kg<p>Cube of Doom®</p>
             </div>
             <div className="cube__face cube__face--top">{props.output}kg</div>
-            <div className="cube__face cube__face--bottom">{props.output}kg</div>
+            <div className="cube__face cube__face--bottom">
+              {props.output}kg
+            </div>
           </div>
         </div>
       </div>
